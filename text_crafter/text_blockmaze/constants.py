276a27abccb29124ba8925045125daafd09886cc
PAULINA STEVIA NOUWOU MINDOM <npaulinastevia@gmail.com>
2023-12-21 20:25:28 -0500

    amend

---
+++ text_crafter/text_blockmaze/constants.py
@@ -0,0 +1,8 @@
+import pathlib
+
+import ruamel.yaml as yaml
+
+root = pathlib.Path(__file__).parent
+for key, value in yaml.safe_load((root / 'data_text.yaml').read_text()).items():
+
+  globals()[key] = value
